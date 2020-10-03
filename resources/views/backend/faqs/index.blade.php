88a21e408429244cf20b091dd8646f67c7d4410e
tqchinh <tqchinh@vnpt.com>
2020-10-03 11:31:29 +0700

    fix show footer

--- resources/views/backend/faqs/index.blade.php
+++ resources/views/backend/faqs/index.blade.php
@@ -51,17 +51,7 @@
         </div><!-- /.box-body -->
     </div><!--box-->
 
-    <!--<div class="box box-info">
-        <div class="box-header with-border">
-            <h3 class="box-title">{{ trans('history.backend.recent_history') }}</h3>
-            <div class="box-tools pull-right">
-                <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-            </div><!-- /.box tools -->
-        </div><!-- /.box-header -->
-        <div class="box-body">
-            {{-- {!! history()->renderType('Blog') !!} --}}
-        </div><!-- /.box-body -->
-    </div><!--box box-info-->
+    {{--    @include('backend.faqs.partials.recent_history')--}}
 @endsection
 
 @section('after-scripts')
--- resources/views/backend/pages/index.blade.php
+++ resources/views/backend/pages/index.blade.php
@@ -47,17 +47,17 @@
         </div><!-- /.box-body -->
     </div><!--box-->
 
-    <!--<div class="box box-info">
-        <div class="box-header with-border">
-            <h3 class="box-title">{{ trans('history.backend.recent_history') }}</h3>
-            <div class="box-tools pull-right">
-                <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-            </div><!-- /.box tools -->
-        </div><!-- /.box-header -->
-        <div class="box-body">
-            {{-- {!! history()->renderType('CMSpage') !!} --}}
-        </div><!-- /.box-body -->
-    </div><!--box box-info-->
+{{--    <!--<div class="box box-info">--}}
+{{--        <div class="box-header with-border">--}}
+{{--            <h3 class="box-title">{{ trans('history.backend.recent_history') }}</h3>--}}
+{{--            <div class="box-tools pull-right">--}}
+{{--                <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>--}}
+{{--            </div><!-- /.box tools -->--}}
+{{--        </div><!-- /.box-header -->--}}
+{{--        <div class="box-body">--}}
+{{--            --}}{{-- {!! history()->renderType('CMSpage') !!} --}}
+{{--        </div><!-- /.box-body -->--}}
+{{--    </div><!--box box-info-->--}}
 @endsection
 
 @section('after-scripts')
